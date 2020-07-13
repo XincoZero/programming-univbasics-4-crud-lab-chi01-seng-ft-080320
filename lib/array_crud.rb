a996d73fbe7ca3ff292f4b09de3bcf47f166a9ba
Hector Alvarez <h-alvarez@wiu.edu>
2020-07-13 00:48:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["John", "Paul", "George", "Ringo"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  new_array ["Nala Cat", "Cat"]
+  << "Garfield" 
 end
 
 def add_element_to_start_of_array(array, element)
